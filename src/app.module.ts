259714ea98b44b82aca60915fdf8327956e3c76c
a.krasovskiy <a.krasovskiy@autodoc.eu>
2022-02-25 15:25:12 +0200

    разработка response transformer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { User } from './users/users.entity';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
-import { Role } from './roles/roles.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import ormConfig from '../db/ormconfig';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: [User, Role],
-      synchronize: true,
-      autoLoadEntities: true,
-    }),
+    TypeOrmModule.forRoot(ormConfig),
     UsersModule,
     RolesModule,
     AuthModule,
