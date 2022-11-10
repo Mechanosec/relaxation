d6013b16a71d173cad555a09aa8b33c09b4bf590
o.krasovskyi <mechanosec29@gmail.com>
2022-11-11 01:54:20 +0200

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,37 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-    extends: [
-        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
-        'plugin:prettier/recommended',
-        'plugin:import/typescript',
-        'plugin:unicorn/recommended',
-    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
-        ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
-        sourceType: 'module', // Allows for the use of imports
+        project: 'tsconfig.json',
+        sourceType: 'module',
     },
-    plugins: ['simple-import-sort', 'import', 'unused-imports', 'unicorn'],
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: ['plugin:@typescript-eslint/eslint-recommended', 'plugin:@typescript-eslint/recommended'],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
     rules: {
-        '@typescript-eslint/explicit-function-return-type': 'error',
-        '@typescript-eslint/explicit-module-boundary-types': 'error',
-        '@typescript-eslint/no-unused-vars': 'error',
-        '@typescript-eslint/no-explicit-any': 'error',
-        'object-curly-newline': 'error',
-        'newline-before-return': 'error',
-        'eol-last': 2,
-        'no-console': ['error', { allow: ['warn', 'error'] }],
-        'no-undef': 'error',
-        'no-fallthrough': 'error',
-        'unused-imports/no-unused-imports-ts': 'error',
-        'import/newline-after-import': 'error',
-        'unicorn/numeric-separators-style': 0,
-        'unicorn/no-null': 0,
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        // "semi": [
+        //   "warning",
+        //   "always"
+        // ],
+        // "@typescript-eslint/quotes": [
+        //   "error",
+        //   "double"
+        // ],
+        // "quotes": [
+        //   "error",
+        //   "double"
+        // ],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        '@typescript-eslint/prefer-optional-chain': ['error'],
+        '@typescript-eslint/switch-exhaustiveness-check': ['error'],
+        '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
     },
 };
